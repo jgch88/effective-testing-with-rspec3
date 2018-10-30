ec907ea53a90c28cc12fe02f6767579e085ee120
jgch88 <radium.laboratories@gmail.com>
2018-10-30 15:03:27 +0800

    01 - using the let construct

--- ch01/01/spec/sandwich_spec.rb
+++ ch01/01/spec/sandwich_spec.rb
@@ -2,29 +2,16 @@ Sandwich = Struct.new(:taste, :toppings)
 # RSpec.describe block is an example group (keeps related tests together, i.e. a sandwich)
 RSpec.describe 'An ideal sandwich' do
 
-  # Using a helper method (plain ol Ruby)
-  def sandwich
-    # Pattern: use memoization to store the result of an operation
-    # and refer to the stored copy from then onwards.
-    # Otherwise sandwich.toppings would refer to a new instance of sandwich and fail the test
-    #
-    # Cons: ||= operator works by seeing if @sandwich is "falsey". It won't work if we're actually
-    # trying to store something falsey.
-    #
-    # e.g.
-    # def current_toaster
-    #   # This would run @current_toaster ||= nil || Toaster.find_by_serial('HHGG42')
-    #   # and not actually memoize anything.
-    #   @current_toaster ||= Toaster.find_by_serial('HHGG42')
-    # end
-    @sandwich ||= Sandwich.new('delicious', [])
-  end
+  # Using RSpec's let construct
+  # let binds a name (sandwich) to the result of a computation (the block)
+  # This block runs the FIRST time any test case calls sandwich
+  let(:sandwich) { Sandwich.new('delicious', []) }
 
   # it block is an example of the sandwich's use, or a test case
   it 'is delicious' do
     # Arrange/Act/Assert pattern
      
-    # Arrange: create Sandwich -> setup in the before hook
+    # Arrange: create Sandwich -> setup in the let construct
     # Act: ask for its taste
     taste = sandwich.taste
     # Assert: verify that result is delicious
