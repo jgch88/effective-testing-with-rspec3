54210bbd4bf642a1e6d5633a6dcebbfb9122afe5
jgch88 <radium.laboratories@gmail.com>
2018-10-30 14:52:51 +0800

    01 - using hooks

--- ch01/01/spec/sandwich_spec.rb
+++ ch01/01/spec/sandwich_spec.rb
@@ -1,24 +1,29 @@
 Sandwich = Struct.new(:taste, :toppings)
 # RSpec.describe block is an example group (keeps related tests together, i.e. a sandwich)
 RSpec.describe 'An ideal sandwich' do
+  # A hook runs automatically at specific times - a before hook runs before each example.
+  # 
+  # Pros: Hooks are great for running common setup code that has real-world SIDE EFFECTS. e.g. setup test databases.
+  # Cons: Having to refactor all sandwich to @sandwich. Misspelling @sandwich - Ruby silently returns nil and a confusing error.
+  # Inefficient and expensive instantiation of all the instance variables
+
+  before { @sandwich = Sandwich.new('delicious', []) } # So that we don't have to copy the sandwich setup code
+  # Individual sandwich instances are not shared across test cases
+
   # it block is an example of the sandwich's use, or a test case
   it 'is delicious' do
     # Arrange/Act/Assert pattern
-    # Arrange: create Sandwich
-    sandwich = Sandwich.new('delicious', [])
-
+    # Arrange: create Sandwich -> setup in the before hook
     # Act: ask for its taste
-    taste = sandwich.taste
+    taste = @sandwich.taste
 
     # Assert: verify that result is delicious
     expect(taste).to eq('delicious')
   end
 
   it 'lets me add toppings' do
-    sandwich = Sandwich.new('delicious', [])
-
-    sandwich.toppings << 'cheese'
-    toppings = sandwich.toppings
+    @sandwich.toppings << 'cheese'
+    toppings = @sandwich.toppings
     
     expect(toppings).not_to be_empty
   end
